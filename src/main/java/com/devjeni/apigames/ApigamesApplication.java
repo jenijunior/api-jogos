a251ee62653bf790b9f82c8ff51ce1d118fad6e9
jenivaldo pereira de souza junior <jenivaldojuniorjr@gmail.com>
2025-08-26 00:13:09 -0300

    project created

---
+++ src/main/java/com/devjeni/apigames/ApigamesApplication.java
@@ -0,0 +1,13 @@
+package com.devjeni.apigames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjeni/apigames/ApigamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjeni.apigames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
